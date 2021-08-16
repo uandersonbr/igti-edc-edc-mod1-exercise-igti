c0187d069b976aa586ba778f166943ea3cd16ce7
Uanderson Oliveira <uandersonbr@gmail.com>
2021-08-16 07:55:40 -0300

    remoção objeto duplicado

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -9,14 +9,6 @@ resource "aws_s3_bucket_object" "job_spark" {
 }
 
 
-resource "aws_s3_bucket_object" "job_spark" {
-  bucket = aws_s3_bucket.dl.id
-  key = "emr-code/pyskpark/job_spark_from_tf.py"
-  acl = "private"
-  source = "../job_spark.py"
-  etag = filemd5("../job_spark.py")
-}
-
 resource "aws_s3_bucket_object" "delta_insert" {
   bucket = aws_s3_bucket.dl.id
   key = "emr-code/pyskpark/01_delta_spark_insert.py"
